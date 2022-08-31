672118d299cefe8570c250d65dd6b6a1dfd23118
Ammanuel Amare <ammanuel14@gmail.com>
2022-08-31 02:47:12 +0000

    MVC Template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 2022fa-420-LigmaPy
+
+1.  pip install -r requirements.txt 
+
+2. 
---
+++ controller.py
@@ -0,0 +1,17 @@
+"""
+    Example Controller
+"""
+from flask import (
+    Blueprint, render_template
+)
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Index page.
+    :return: The response.
+    """
+
+    return render_template('views/main.html')
