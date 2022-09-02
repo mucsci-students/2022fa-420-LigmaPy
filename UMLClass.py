918646197ca662a882b726b404b65b0234a1b6b2
sam <sjnoggle@millersville.edu>
2022-09-01 22:31:20 -0400

    fixed edge case of renaming with an already used name

--- UMLClass.py
+++ UMLClass.py
@@ -13,11 +13,14 @@ class UMLClass:
         self.attributes = []
 
     def rename(self, newName):
-        print(f"Class \"{self.name}\" was renamed to \"{newName}\"")
+        print(f"\nClass \"{self.name}\" was renamed to \"{newName}\"")
         self.name = newName
 
 
 def isNameUnique(name: str):
+    '''
+    Checks classIndex for duplicate names
+    '''
     for c in classIndex:
         if c.name == name:
             return False
@@ -26,48 +29,57 @@ def isNameUnique(name: str):
 
 def findClass(name: str):
     """
-    Finds a class object by name
-
-    :param name: the name of the class to find
-    :param classes: the list of all class objects
-
-    :return: the index of the class object with the specified name
-            or None if it was not found
+    Finds a class object by name and returns it's index
     """
     for i, c in enumerate(classIndex):
         if c.name == name:
             return i
-    print(f"Class \"{name}\" does not exist")
+    print(f"\nClass \"{name}\" does not exist")
     return None
 
 
 def addClass(name: str):
+    """
+    Creates and adds a new class 
+    """
     if isNameUnique(name):
         newClass = UMLClass(name)
         classIndex.append(newClass)
-        print(f"{newClass.name} has been created!")
+        print(f"\nClass \"{newClass.name}\" has been created!")
     else:
-        print(f"{name} already exists, could not create.")
+        print(f"\nClass \"{name}\" already exists, could not create.")
 
 
 def deleteClass(name: str):
+    """ 
+    Deletes a class by it's name
+    """
     index = findClass(name)
     if index is not None:
         classIndex.pop(index)
-        print(f"Class \"{name}\" has been deleted.")
+        print(f"\nClass \"{name}\" has been deleted.")
     else:
-        print(f"Deletion failed")
+        print("Deletion failed")
 
 
 def renameClass(oldName: str, newName: str):
+    """ 
+    Renames a class from oldName to newName
+    """
+    if findClass(newName):
+        print(f"\nA class already exists with the name \"{newName}\"")
+        print("Rename failed")
+        return
+
     index = findClass(oldName)
     if index is not None:
         classIndex[index].rename(newName)
     else:
-        print(f"Rename failed")
+        print("Rename failed")
 
 
-###### Driver Code ######
+######################## Driver Code ########################
+
 def main():
 
     # Not yet an exit function so it'll wait for commands forever
@@ -88,10 +100,10 @@ def main():
             # Error
             else:
                 print(
-                    f"\n {command[0]} is not a command or too many arguments were passed.")
+                    f"\n\"{command[0]}\" is not a command or too many arguments were passed.")
                 print("Type \"help\" for command syntax.")
         else:
-            print("Not enough arguments.")
+            print("\nNot enough arguments.")
             print("Type \"help\" for command syntax.")
 
 
