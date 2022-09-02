fb5cd9e2d494700f8786d922530a9b25f845650f
Spyder-Monkey <tbender@ptd.net>
2022-09-02 11:00:03 -0400

    Added placeholders to listClasses, listClass and listRelationships command.

--- interface.py
+++ interface.py
@@ -1,3 +1,10 @@
+"""
+    Trevor Bender & Christian Sheperdson
+
+    Basic CLI interface and commands to go
+    along with it.
+"""
+
 import json
 
 class Interface():
@@ -11,9 +18,24 @@ class Interface():
         corresponding commands.
     '''
     def run(self):
-        self.welcomeMsg()
+        self.__welcomeMsg()
         while self.isRunning:
             cmd = input(">> ").split(" ")
+            # List all classes and contents
+            if cmd[0] == 'listClasses':
+                self.listClasses()
+            # List contents of a specified class
+            if cmd[0] == 'listClass':
+                if len(cmd) < 2:
+                    print(f"Not enough arguments.\nType 'help' {cmd[0]} for correct usage.")
+                    continue
+                elif len(cmd) > 2:
+                    print(f"Too many arguments.\n Type 'help' {cmd[0]} for correct usage.")
+                    continue
+                self.listClass(cmd[1])
+            # List all relationships that exist between classes
+            if cmd[0] == 'listRelationships':
+                self.listRelationships()
             # Help with a specified command
             if cmd[0] == 'help' and len(cmd) > 1:
                 self.help(cmd[1])
@@ -25,10 +47,11 @@ class Interface():
                 self.exit()
 
     '''
+        Private
         Shows a formatted welcome message to user. Only shown
         at the startup of the program.
     '''
-    def welcomeMsg(self):
+    def __welcomeMsg(self):
         print("\t*****************************************")
         print("\t*\t\t\t\t\t*")
         print("\t*\t\tUML Editor\t\t*")
@@ -41,16 +64,34 @@ class Interface():
     '''
         Lists all of the classes and contents
         of the classes in a nice way.
+
+        Example Output:
+
+        Class Name:
+            Attribute
+            Attribute
+            ...
     '''
     def listClasses(self):
-        pass
+        # Placeholder
+        print("ClassName:")
+        print("\tAttribute\n\tAttribute")
+
+        print("ClassName:")
+        print("\tAttribute\n\tAttribute")
 
     '''
         Lists the contents of a specified
         class in a nice way.
     '''
     def listClass(self, name):
-        pass
+        print(f"\n {name} Attributes")
+        # Loop to print bottom border with 
+        # length len(name) + len("Attributes") + 2
+        for i in range((len(name) + 13)):
+            print("*", end="")
+        # Placeholder
+        print("\nAttribute 1\nAttribute 2\n...\nAttribute n\n")
 
     '''
         Lists all of the relationships that
@@ -60,7 +101,11 @@ class Interface():
         [src] -> [dest]
     '''
     def listRelationships(self):
-        pass
+        # Placeholder
+        print("[src 1] -> [dest 1]")
+        print("[src 2] -> [dest 2]")
+        print("...")
+        print("[src n] -> [dest n]")
 
     '''
         Lists how to use the application without
@@ -76,7 +121,6 @@ class Interface():
             print("***************************************************************************************")
             for i in cmds['commands']:
                 newLine = '{:<20}{:<12}'.format(i['name'], i['desc'])
-                # print(i['name'] + "\t" + i['desc'] + "\n")
                 print(newLine)
             print("***************************************************************************************")
         else:
@@ -87,14 +131,15 @@ class Interface():
             for name in cmds["commands"]:
                 if cmd == name["name"]: 
                     command = name
+                    break
             # Prints the usage and description of the specified command.
             if command is not None:
                 print("Usage: " + cmd, end="")
                 for option in command["args"]:
-                    print(" [" + option + "]", end="")
-                print("\n\n\t" + command["desc"] + "\n")
+                    print(f" [{option}]", end="")
+                print(f"\n\n\t{command['desc']}\n")
             else:
-                print("Invalid command. Type 'help' for a list of commands.")
+                print("Command not found. Type 'help' for a list of commands.")
                 
 
 
