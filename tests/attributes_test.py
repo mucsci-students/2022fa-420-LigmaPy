b21cba500938be92f71f3502365d8997478944f2
Trevor Bender <trevor.bender99@gmail.com>
2022-10-06 16:23:51 -0400

    Finished basic tests and added comments

--- tests/attributes_test.py
+++ tests/attributes_test.py
@@ -1,48 +1,72 @@
+"""
+Author(s)   : Trevor Bender
+Filename    : attributes_test.py
+Description : Tests for the attributes class
+"""
+
 import UMLClass
 # Class to test
 import attributes
 
 UMLClass.addClass("Tire")
 
-"""
-    Field Tests
-"""
+"""    Add Field Tests     """
 def test_add_field():
+    # Test adding a field to a class
     field = attributes.addField("psi", "Tire", "float")
     assert field == 1
 
 def test_add_to_nonexisting_class():
+    # Test adding a field to a class that does not exist
     assert attributes.addField("psi", "Car", "float") == -1
 
 def test_add_existing_field():
+    # Test adding a field that already exists
     attributes.addField("psi", "Tire", "float")
     assert attributes.addField("psi", "Tire", "float") == -2
 
+"""     Rename Field Tests    """
 def test_rename_field():
+    # Test renaming a field
     attributes.addField("brand", "Tire", "string")
     assert attributes.renameField("brand", "manufacturer", "Tire") == 1
 
+"""     Delete Field Tests    """
 def test_delete_nonexisting_field():
-    assert attributes.deleteField("manufacturer", "Tire") == -2
+    # Test deleting a field that does not exist
+    assert attributes.deleteField("creator", "Tire") == -2
 
 def test_delete_field_nonexisting_class():
+    # Test deleting a field in a class that does not exist
     attributes.addField("psi", "Tire", "float")
     assert attributes.deleteField("psi", "tire") == -1
 
 def test_delete_field():
+    # Test deleting a field
     attributes.addField("psi", "Tire", "float")
     assert attributes.deleteField("psi", "Tire") == 1
 
-"""
-    Method Tests
-"""
+"""     Add Method Tests     """
 def test_add_method():
-    assert attributes.addMethod("setPSI", "Tire", "void") == 1
+    # Test adding a method to a class
+    assert attributes.addMethod("setDiameter", "Tire", "void") == 1
 
+"""     Rename Method Tests     """
 def test_rename_method():
+    # Test renaming a method
     attributes.addMethod("getBarnd", "Tire", "string")
     assert attributes.renameMethod("getBarnd", "getBrand", "Tire") == 1
-
+"""     Delete Method Tests    """
 def test_delete_method():
+    # Test deleting a method from a class
+    attributes.addMethod("getBrand", "Tire", "string")
     assert attributes.deleteMethod("getBrand", "Tire") == 1
+
+def test_delete_method_nonexisting_class():
+    # Test deleting a method from a class that does not exist
+    assert attributes.deleteMethod("setPSI", "Car") == -1
+
+def test_delete_nonexisting_method():
+    # Test deleting a method that does not exist
+    assert attributes.deleteMethod("getDiameter", "Tire") == -2
 
--- tests/parameter_test.py
+++ tests/parameter_test.py
@@ -1,3 +1,9 @@
+"""
+Author(s)   : Trevor Bender
+Filename    : parameter_test.py
+Description : Tests for the parameter class
+"""
+
 import UMLClass
 import attributes
 # Class to test
@@ -7,40 +13,42 @@ UMLClass.addClass("Tire")
 attributes.addMethod("setPSI", "Tire", "void")
 attributes.addMethod("getQuality", "Tire", "string")
 
+"""     Add Parameter Tests     """
 def test_add_parameter():
+    # Test adding a parameter
     assert parameter.addParameter([("new_psi", "string")], "setPSI", "Tire") == 1
 
 def test_add_multiple_parameters():
+    # Test adding multiple parameters at once
     assert parameter.addParameter([("currentTread", "Float"), ("illegalTread", "Float")], "getQuality", "Tire") == 1
 
 def test_add_duplicate_parameters():
+    # Test adding a parameter that already exists
     parameter.addParameter([("new_psi", "string")], "setPSI", "Tire")
     assert parameter.addParameter([("new_psi", "string")], "setPSI", "Tire") == -3
 
 def test_method_does_not_exist():
+    # Test adding parameters to a method that does not exist
     assert parameter.addParameter([("velocity", "float")], "getRotation", "Tire") == -2
 
 def test_class_does_not_exist():
+    # Test adding a parameter to a method when the class does not exist
     assert parameter.addParameter([("currentTread", "Float")], "getQuality", "Engine") == -1
 
+"""     Delete Parameter Tests     """
 def test_delete_single_parameter():
+    # Test deleting a single parameter
     parameter.addParameter([("fudge", "long")], "setPSI", "Tire")
     assert parameter.deleteParameter(["fudge"], "setPSI", "Tire") == 1
 
-def test_delete_multiple_parameters():
-    parameter.addParameter([("new_psi", "string"), ("blaahhh", "string")], "setPSI", "Tire")
-    assert parameter.deleteParameter(["new_psi", "blaahhh"], "setPSI", "Tire") == 1
-
 def test_delete_all_parameters():
+    # Test deleting all parameters at once
     parameter.addParameter([("bus", "string"), ("earned", "int"), ("idNum", "string")], "getQuality", "Tire")
     assert parameter.deleteAllParameter("getQuality", "Tire") == 1
 
+"""     Change Parameter Tests     """
 def test_change_parameters():
+    # Test Changing the name of a parameter
     parameter.addParameter([("new_pis", "float")], "setPSI", "Tire")
     changed = parameter.changeParameter([("new_pis")], [("new_psi")], "setPSI", "Tire")
     assert changed ==  1
-
-def test_change_multiple_parameters():
-    parameter.addParameter([("new_pis", "float"), ("max_pis", "float"), ("diameter", "float")], "setPSI", "Tire")
-    changed = parameter.changeParameter([("new_pis"), ("max_pis")], [("new_psi"), ("max_psi")], "setPSI", "Tire")
-    assert changed == 1
